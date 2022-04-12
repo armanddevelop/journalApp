c45a1d352510b8893ddd9736edc63e425ffe41ff
armando salamanca ayon <asalamanca.ayon@gmail.com>
2022-04-12 18:07:12 -0500

    add config environment variables

--- src/FireBase/fireBaseConfig.js
+++ src/FireBase/fireBaseConfig.js
@@ -1,33 +1,17 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDxyp1015N4zn08p55G19dcUd9oIQaoBDg",
-  authDomain: "react-app-journal-b7fd3.firebaseapp.com",
-  projectId: "react-app-journal-b7fd3",
-  storageBucket: "react-app-journal-b7fd3.appspot.com",
-  messagingSenderId: "619240223461",
-  appId: "1:619240223461:web:332d75c1709a89b8e95a0c",
-};
 // Your web app's Firebase configuration test
-const firebaseConfigTest = {
-  apiKey: "AIzaSyBD3Xm_fYAd9jygyO5O0f-XVjfh7JJQky0",
-  authDomain: "react-jornal-app-testing.firebaseapp.com",
-  projectId: "react-jornal-app-testing",
-  storageBucket: "react-jornal-app-testing.appspot.com",
-  messagingSenderId: "317704425168",
-  appId: "1:317704425168:web:c88432d0f3a3d6f09ad8d5",
-  measurementId: "G-6S9650Y4G4",
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
-if (process.env.NODE_ENV === "test") {
-  // Initialize Firebase test enviroment
-  firebase.initializeApp(firebaseConfigTest);
-} else {
-  // Initialize Firebase prod
-  firebase.initializeApp(firebaseConfig);
-}
+firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
