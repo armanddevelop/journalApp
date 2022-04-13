610674daa504a4a91341b254723d0c5f7063223b
armando salamanca ayon <asalamanca.ayon@gmail.com>
2022-04-13 18:02:41 -0500

    add unit test to notes action file and create the conexion to firebase to test data

--- src/Helpers/loadNotes.js
+++ src/Helpers/loadNotes.js
@@ -1,13 +1,17 @@
 import { db } from "../FireBase/fireBaseConfig";
 
 export const loadNotes = async (uid) => {
-  const notesSnap = await db.collection(`${uid}/jornal/notes`).get();
-  const notes = [];
-  notesSnap.forEach((snapChild) => {
-    notes.push({
-      id: snapChild.id,
-      ...snapChild.data(),
+  try {
+    const notesSnap = await db.collection(`${uid}/jornal/notes`).get();
+    const notes = [];
+    notesSnap.forEach((snapChild) => {
+      notes.push({
+        id: snapChild.id,
+        ...snapChild.data(),
+      });
     });
-  });
-  return notes;
+    return notes;
+  } catch (error) {
+    console.log("shit happends in loadNotes ", error);
+  }
 };
--- src/tests/Actions/notes.spec.js
+++ src/tests/Actions/notes.spec.js
@@ -1,18 +1,38 @@
+/**
+ * @jest-environment node
+ */
 import configureStore from "redux-mock-store";
 import thunk from "redux-thunk";
-import { startNewNoteAction } from "../../Actions/notes";
+import {
+  startNewNoteAction,
+  notesActiveAction,
+  starLoadingNotesAction,
+  startSaveNoteAction,
+} from "../../Actions/notes";
 import { db } from "../../FireBase/fireBaseConfig";
 import { types } from "../../Types/types";
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
-const store = mockStore({
+const initState = {
   auth: {
     uid: "TESTING",
   },
-});
+  notes: {
+    active: {
+      title: "",
+      body: "",
+    },
+    notes: [],
+  },
+};
+let store = mockStore(initState);
 
 describe("Test on notes actions", () => {
+  beforeEach(() => {
+    store = mockStore(initState);
+  });
+
   it("startNewNoteAction", async () => {
     await store.dispatch(startNewNoteAction());
     const actions = store.getActions();
@@ -43,5 +63,51 @@ describe("Test on notes actions", () => {
     const { id } = payload;
     const { auth } = store.getState((state) => state);
     await db.doc(`${auth.uid}/jornal/notes/${id}`).delete();
+  });
+
+  it("notesActiveAction test", () => {
+    const id = 123456;
+    const note = {
+      title: "titulo",
+      body: "esto es una nota",
+      date: 1649798985378,
+    };
+    const responseAction = notesActiveAction(id, note);
+    const { type, payload } = responseAction;
+
+    expect(type).toEqual(types.notesActive);
+    note.id = id;
+    expect(payload).toEqual(note);
+  });
+
+  it("starLoadingNotesAction should load the notes", async () => {
+    const { auth } = store.getState((state) => state);
+    await store.dispatch(starLoadingNotesAction(auth.uid));
+    const actions = store.getActions();
+    const expectedObject = {
+      id: expect.any(String),
+      title: expect.any(String),
+      body: expect.any(String),
+      date: expect.any(Number),
+    };
+
+    expect(actions[0].length).not.toBe(0);
+    expect(actions[0].type).toBe(types.notesLoad);
+    expect(actions[0].payload[0]).toMatchObject(expectedObject);
+  });
+
+  it("startSaveNoteAction should save a note", async () => {
+    const note = {
+      id: "Cjx6qYhnGuCQ4IeoUwJX",
+      title: "hola creting",
+      body: "esta es una nota actualizada",
+      date: 1649798707452,
+    };
+    await store.dispatch(startSaveNoteAction(note));
+    const actions = store.getActions();
+
+    expect(actions[2].type).toEqual(types.notesOpenModal);
+    expect(actions[2].payload.strMsg).toEqual("saved");
+    expect(actions[2].payload.strTitle).toEqual(note.title);
   });
 });
